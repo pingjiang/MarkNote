0db898ec448b5063b397cae9f0667da5f2d9bca3
pingjiang <pingjiang1989@gmail.com>
2014-08-31 15:23:07 +0800

    Initial Commit

---
+++ MarkNote/PJDocument.h
@@ -0,0 +1,13 @@
+//
+//  PJDocument.h
+//  MarkNote
+//
+//  Created by 平江 on 14-8-31.
+//  Copyright (c) 2014年 平江. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PJDocument : NSDocument
+
+@end
---
+++ MarkNoteTests/MarkNoteTests.m
@@ -0,0 +1,34 @@
+//
+//  MarkNoteTests.m
+//  MarkNoteTests
+//
+//  Created by 平江 on 14-8-31.
+//  Copyright (c) 2014年 平江. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MarkNoteTests : XCTestCase
+
+@end
+
+@implementation MarkNoteTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
